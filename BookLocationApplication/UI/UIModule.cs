1edfae41e1cd6d9a4c09a8941fef260245d14227
he_yuan_tao <he_yuan_tao@163.com>
2016-01-19 11:21:09 +0800

    begin shell

--- BookLocationApplication/UI/UIModule.cs
+++ BookLocationApplication/UI/UIModule.cs
@@ -1,22 +1,31 @@
 ﻿using Microsoft.Practices.Unity;
 using Prism.Modularity;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UI.Views;
 
 namespace UI
 {
     public class UIModule:IModule
     {
         IUnityContainer container;
-        public UIModule(IUnityContainer container)
+        IRegionManager regionManager;
+        public UIModule(IUnityContainer container, IRegionManager regionManager)
         {
             this.container = container;
+            this.regionManager = regionManager;
         }
         public void Initialize()
         {
+            container.RegisterType<NavBar, NavBar>();
+            container.RegisterType<ShowBookLocation, ShowBookLocation>();
+            container.RegisterType<SystemSetting, SystemSetting>();
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(SystemSetting));
+            regionManager.RegisterViewWithRegion("NavRegion", typeof(NavBar));
         }
     }
 }
