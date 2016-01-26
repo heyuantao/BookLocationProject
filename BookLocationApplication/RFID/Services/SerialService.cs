e5532b044d87f4c49b1d1d65a92a781d9d6676b9
he_yuan_tao <he_yuan_tao@163.com>
2016-01-26 16:46:39 +0800

    serial list

---
+++ BookLocationApplication/RFID/Services/SerialService.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure;
+using Microsoft.Practices.Unity;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFID
+{
+    public class SerialService : ISerialService
+    {
+        IUnityContainer container;
+        IEventAggregator eventAggregator;
+        public SerialService(IUnityContainer container)
+        {
+            this.container = container;
+            this.eventAggregator = container.Resolve<IEventAggregator>();
+
+        }
+        public List<String> SerialList()
+        {
+            string[] PortListInString;
+            List<String> PortList = new List<String>();
+            try
+            {
+                PortListInString = SerialPort.GetPortNames();
+                foreach (String item in PortListInString)
+                {
+                    PortList.Add(item);
+                }
+                return PortList;
+            }
+            catch (Exception)
+            {
+                eventAggregator.GetEvent<RFIDHardwareEvent>().Publish("获取串口列表失败！");
+                return new List<String>();
+            }
+        }
+    }
+}
