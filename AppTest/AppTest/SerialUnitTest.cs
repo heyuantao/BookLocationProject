f30c89fb25ce7349e65d4df107b34d86f06a78bd
he_yuan_tao <he_yuan_tao@163.com>
2016-01-26 16:54:21 +0800

    serial no test

---
+++ AppTest/AppTest/SerialUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTest
+{
+    class SerialUnitTest
+    {
+        public static void testSerial(IUnityContainer container)
+        {
+
+        }
+    }
+}
