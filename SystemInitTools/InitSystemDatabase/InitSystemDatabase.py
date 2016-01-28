ef414435a1d9617556878bc187700685332c1fd0
he_yuan_tao <he_yuan_tao@163.com>
2016-01-28 11:31:01 +0800

    add new database

---
+++ SystemInitTools/InitSystemDatabase/InitSystemDatabase.py
@@ -0,0 +1,37 @@
+import pymssql
+
+def testDatabaseInsert(databaseConnection):
+    cursor=databaseConnection.cursor()
+    cursor.execute("insert into TestTable (testKey,testValue) values ('this','go')")
+    databaseConnection.commit()
+    print "Insert Success !"
+
+def testDatabaseSelect(databaseConnection):
+    cursor=databaseConnection.cursor()
+    cursor.execute("select * from TestTable")
+    print "Begin display"
+    for row in cursor:
+        print row
+    print "End display"
+
+def testDatabaseOperation():
+    servername="DESKTOP-PSQP38H"
+    username="sa"
+    password="19831122"
+    databasename="TestDatabase"
+    #database connection
+    print "Connect Databaes !"
+    connection = pymssql.connect(server=servername, user=username, password=password, database=databasename)
+    ###insert data
+    testDatabaseInsert(connection)
+    ###display data
+    testDatabaseSelect(connection)
+    ##close database
+    connection.close()
+    print "Close Database !"
+
+
+if __name__=="__main__":
+    #this code is use for test database function,common it
+    #testDatabaseOperation()
+    pass
---
+++ SystemInitTools/SystemInitTools/SystemInitTools.py
@@ -0,0 +1,4 @@
+
+from InitSystemDatabase import initDatabase
+if __name__=="__main__":
+    InitSystemDatabase.initDatabase()
