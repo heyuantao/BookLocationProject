3165f18ee563951b6ec2ad824d5fef5620c99a05
he_yuan_tao <he_yuan_tao@163.com>
2016-02-01 15:15:10 +0800

    canvas test

---
+++ CanvasTest/CanvasTest/TestVM.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CanvasTest
+{
+    public class TestVM
+    {
+        Canvas canvas;
+        ICommand click;
+        public TestVM()
+        {
+            
+        }
+        public ICommand Click
+        {
+            get
+            {
+                if (this.click == null)
+                {
+                    this.click = new DelegateCommand(onClickExecute);
+                }
+                return this.click;
+            }
+        }
+
+        private void onClickExecute()
+        {
+            Random rnd = new Random();
+            Canvas canvas = this.canvas;
+            Rectangle rect = new Rectangle();
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.Fill = new SolidColorBrush(Colors.Black);
+            rect.Width = 30;
+            rect.Height = 30;
+            Canvas.SetLeft(rect, rnd.Next(1,1000));
+            Canvas.SetTop(rect, rnd.Next(1, 1000));
+            canvas.Children.Add(rect);
+        }
+        public Canvas MyCanvas
+        {
+            get
+            {
+                if (this.canvas == null)
+                {
+                    this.canvas = new Canvas();
+                    //this.canvas.Height = 400;
+                    //this.canvas.Width = 300;
+                    this.canvas.Background = new SolidColorBrush(Colors.Blue);
+                }
+                return this.canvas;
+            }
+        }
+    }
+}
