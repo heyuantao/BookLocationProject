f335332e4ce3fe3fae657f68e511763966c4463e
he_yuan_tao <he_yuan_tao@163.com>
2016-02-03 13:42:13 +0800

    datagrid height

--- BookLocationApplication/UI/Views/BookLocationShowView.xaml.cs
+++ BookLocationApplication/UI/Views/BookLocationShowView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ using UI.ViewModels;
 
 namespace UI.Views
 {
+
     /// <summary>
     /// Interaction logic for BookLocationShowView.xaml
     /// </summary>
--- BookLocationApplication/UI/Views/RecodeBookLocationView.xaml.cs
+++ BookLocationApplication/UI/Views/RecodeBookLocationView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,22 @@ using UI.ViewModels;
 
 namespace UI.Views
 {
+    public class MyConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType,
+               object parameter, CultureInfo culture)
+        {
+            return (double)value - 80;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+               object parameter, CultureInfo culture)
+        {
+            return (double)value + 80;
+        }
+
+    }
     /// <summary>
     /// Interaction logic for RecodeBookLocationView.xaml
     /// </summary>
