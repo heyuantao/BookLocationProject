a197e5acffa53cf278f40665231fc8f404727670
heyuantao <he_yuan_tao@163.com>
2016-02-04 12:35:19 +0800

    continue on draw

--- BookLocationApplication/UI/Services/DrawMapService.cs
+++ BookLocationApplication/UI/Services/DrawMapService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,55 @@ namespace UI.Services
     //此服务注入容器
     public class DrawMapService
     {
-        CanvasDrawer oneShelfDrawer;
-        CanvasDrawer shelfMapDrawer;
-        //List<ShelfShape> 
+        IUnityContainer container;
 
-        public DrawMapService()
+        CanvasDrawer oneShelfDrawer;
+        CanvasDrawer libraryShelfDrawer;
+
+        List<ShelfShape> oneShelfBoxList;  //书架正视图中的几层的矩形图
+        ContourShape oneShelfBoxContour;     //书架正视图的外部轮廓
+
+        List<ShelfShape> shelfMapShelfList; //一个书库俯视图中所有书柜的列表
+        DoorShape shelfMapDoor;             //一个书库门的位置
+        ContourShape shelfMapContour;       //一个书库的外部轮廓
+        RouteShape shelfMapRoute;           //取书的路径列表
+
+        public DrawMapService(IUnityContainer container)
         {
+            this.container = container;
+            //初始化其他变量
             this.oneShelfDrawer = new CanvasDrawer();
-            this.shelfMapDrawer = new CanvasDrawer();
+            this.libraryShelfDrawer = new CanvasDrawer();
+        }
+        public Canvas OneShelfMap
+        {
+            get { return this.oneShelfDrawer.CurrentCanvas; }
+        }
+        public Canvas LibraryShelfMap
+        {
+            get { return this.libraryShelfDrawer.CurrentCanvas; }
+        }
+
+        //给上层提供的访问函数
+        //该函数能够画出一个书架的正视图，参数为层数。也就是画出几层的书架
+        public void drawOneShapeMapBackground(int layers)
+        {
+
+        }
+        //该函数能够在一个已有的书架正视图上画出被选中的书架层
+        public void drawSelectedLayerOneShapeMap(int layer)
+        {
+
+        }
+        //该函数能够画出一个书库的背景图片，包含书架，轮廓和门，参数为该图书馆名称，参数即为Map表中的location字段
+        public void LibraryShelfMapBackgroundByLibraryName(String libraryname)
+        {
+
+        }
+        //该函数能够画出取书的路线，也就是从入口到目的书架的折线图，参数为Map表中的rfidOfShelf字段
+        public void drawRouteOnLibraryShelfMap(String shelfRfid)
+        {
+
         }
     }
     
