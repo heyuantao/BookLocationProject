eb5d8670b259a72059d7b604ee3f6c2b6f7ebccb
he_yuan_tao <he_yuan_tao@163.com>
2016-02-10 15:23:17 +0800

    change

--- BookLocationApplication/UI/ViewModels/WrongBookLocationViewModel.cs
+++ BookLocationApplication/UI/ViewModels/WrongBookLocationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Infrastructure;
 using Microsoft.Practices.Unity;
+using Prism.Commands;
 using Prism.Events;
 using Prism.Regions;
 using System;
@@ -13,7 +14,7 @@ using System.Windows.Input;
 
 namespace UI.ViewModels
 {
-    class BookItemOfWrongLocation //这个数据结构用于组成一个ObservableCollection<BookItem>()
+    public class BookItemOfWrongLocation //这个数据结构用于组成一个ObservableCollection<BookItem>()
     {//用于在ui界面中显示一个列表型的图书信息
         public String ID { get; set; }  //按递增顺序排列
         public String BookName { get; set; }
@@ -29,8 +30,8 @@ namespace UI.ViewModels
         //该控件使用到的变量
         String shelfRfid = "";//扫描到的书架的rfid标签，用于该模块内部逻辑使用
         String shelfLocationString = "";//用于在UI中显示的"当前书架位置"
-        ObservableCollection<BookItemOfWrongLocation> onTheShelfBookList;
-        ObservableCollection<BookItemOfWrongLocation> notOnTheShelfBookList;
+        ObservableCollection<BookItemOfWrongLocation> onThisShelfAllBookList;
+        ObservableCollection<BookItemOfWrongLocation> notOnThisShelfBookList;
         ICommand wrongBookLocationCleanAllCommand; //对应按钮的事件
 
         public WrongBookLocationViewModel(IUnityContainer container, IRegionManager regionManager)
@@ -40,11 +41,71 @@ namespace UI.ViewModels
             this.eventAggregator = container.Resolve<IEventAggregator>();
             this.dispatcherService = container.Resolve<IDispatcherService>();
             //初始化控件用到的变量
-            this.onTheShelfBookList = new ObservableCollection<BookItemOfWrongLocation>();
-            this.notOnTheShelfBookList = new ObservableCollection<BookItemOfWrongLocation>();
-
+            this.onThisShelfAllBookList = new ObservableCollection<BookItemOfWrongLocation>();
+            this.notOnThisShelfBookList = new ObservableCollection<BookItemOfWrongLocation>();
+            this.wrongBookLocationCleanAllCommand = new DelegateCommand(wrongBookLocationCleanAllCommandExecute, wrongBookLocationCleanAllCommandCanExecute);
         }
 
+        private bool wrongBookLocationCleanAllCommandCanExecute()
+        {
+            //判断清除按钮是否可以被激活
+            if (! String.IsNullOrEmpty(this.shelfLocationString))
+            {
+                return true;
+            }
+            if (this.onThisShelfAllBookList.Count() > 0)
+            {
+                return true;
+            }
+            if (this.notOnThisShelfBookList.Count() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void wrongBookLocationCleanAllCommandExecute()
+        {
+            throw new NotImplementedException();
+        }
+        //Bind variable and Icommand on UI
+        public String ShelfLocationString
+        {
+            get { return this.shelfLocationString; }
+            set {
+                this.shelfLocationString = value; this.OnPropertyChanged("ShelfLocationString");
+                this.dispatcherService.Dispatch(() =>
+                {
+                    ((DelegateCommand)this.WrongBookLocationCleanAllCommand).RaiseCanExecuteChanged();
+                });
+            }
+        }
+        public ICommand WrongBookLocationCleanAllCommand
+        {
+            get { return this.wrongBookLocationCleanAllCommand; }
+        }
+        public ObservableCollection<BookItemOfWrongLocation> OnThisShelfAllBookList
+        {
+            get { return this.onThisShelfAllBookList; }
+            set { 
+                this.onThisShelfAllBookList = value; this.OnPropertyChanged("OnThisShelfAllBookList");
+                this.dispatcherService.Dispatch(() =>
+                {
+                    ((DelegateCommand)this.WrongBookLocationCleanAllCommand).RaiseCanExecuteChanged();
+                });
+            }
+        }
+        public ObservableCollection<BookItemOfWrongLocation> NotOnThisShelfBookList
+        {
+            get { return this.notOnThisShelfBookList; }
+            set { 
+                this.notOnThisShelfBookList = value; this.OnPropertyChanged("NotOnThisShelfBookList");
+                this.dispatcherService.Dispatch(() =>
+                {
+                    ((DelegateCommand)this.WrongBookLocationCleanAllCommand).RaiseCanExecuteChanged();
+                });
+            }
+        }
 
         //########################################
         //########################################
